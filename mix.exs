6952ec84c122b4e56b92cca4d2272e341f08fbd5
Ben Marx <bmarx@bleacherreport.com>
2017-10-29 17:33:50 -0700

    add license, hex info

--- mix.exs
+++ mix.exs
@@ -5,10 +5,13 @@ defmodule Xyz.Mixfile do
     [
       app: :xyz,
       deps: deps(),
+      description: description(),
       dialyzer: [
         plt_add_deps: true
       ],
       elixir: "~> 1.5",
+      name: "Xyz",
+      package: package(),
       preferred_cli_env: ["coveralls": :test,
                           "coveralls.detail": :test,
                           "coveralls.post": :test,
@@ -36,6 +39,19 @@ defmodule Xyz.Mixfile do
       {:ex_doc, "~> 0.18.1",  only: [:dev], runtime: false},
       {:inch_ex, "~> 0.5.6", only: [:dev], runtime: false},
       {:plug, "~> 1.4.3"}
+    ]
+  end
+
+  defp description() do
+    "Gzip response if gzip is sent as a value in the accept-encoding header"
+  end
+
+  defp package() do
+    [
+      files: ["lib",  "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Ben Marx"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/bgmarx/xyz"}
     ]
   end
 end
